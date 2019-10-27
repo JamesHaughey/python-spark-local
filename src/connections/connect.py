42df910e94dd56545e23c5db93c48519907792cc
James Haughey <jimmyhaugh@googlemail.com>
2019-10-27 11:40:12 +0000

    fix ps_connection to work with existing connections

--- src/connections/connect.py
+++ src/connections/connect.py
@@ -1,4 +1,5 @@
 from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
 
 
 class ps_connection(object):
@@ -7,8 +8,8 @@ class ps_connection(object):
         self._log_level = log_level
         self._cores = cores
         self._conf = SparkConf().setAppName(self._app_name).setMaster(f"local[{self._cores}]")
-        self.sc = SparkContext(conf=self._conf)
+        self.sc = SparkContext.getOrCreate(conf=self._conf)
         self.sc.setLogLevel(self._log_level)
     
     def __repr__(self):
-        return f"ps_connection(app_name = {self._app_name}, log_level = {self._log_level}, cores= {self._cores})"
+        return f"ps_connection(app_name = '{self._app_name}', log_level = '{self._log_level}', cores= {self._cores})"
