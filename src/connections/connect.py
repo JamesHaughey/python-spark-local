ff42925f3464d80e076667e765e2184595278223
James Haughey <jimmyhaugh@googlemail.com>
2019-10-27 09:16:14 +0000

    add basic pyspark connection class

---
+++ src/connections/connect.py
@@ -0,0 +1,13 @@
+from pyspark import SparkContext, SparkConf
+
+
+class ps_connection(object):
+    def __init__(self, app_name, log_level, cores):
+        self._app_name = app_name
+        self._log_level = log_level
+        self._cores = cores
+        self._conf = SparkConf().setAppName(self._app_name).setMaster(f"local[{self._cores}]")
+        self.sc = SparkContext(conf=self._conf)
+    
+    def __repr__(self):
+        return f"ps_connection(app_name = {self._app_name}, log_level = {self._log_level}, cores= {self._cores})"
